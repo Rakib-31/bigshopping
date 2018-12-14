9a095ce6dc29ef0e32674b99d272808c4e1b7bc4
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-12-15 04:18:13 +0600

    Displaying categories and products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,7 +58,7 @@ import { CustomFormsModule } from 'ng2-validation';
     AngularFireAuthModule,
     AngularFireDatabaseModule,
     RouterModule.forRoot([
-      {path: '', component: HomeComponent},
+      {path: '', component: ProductsComponent},
       {path:'products', component: ProductsComponent},
       {path:'shopping-cart', component: ShoppingCartComponent},
       {path:'login', component: LoginComponent},
--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent{
+  products$;
+  categories$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private productService: ProductService,
+    categoryService: CategoryService) { 
+    this.products$ = productService.getAll();
+    this.categories$ = categoryService.getAll();
   }
 
 }
