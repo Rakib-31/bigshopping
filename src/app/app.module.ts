807c49fe67c8b66de05e1892dc3db87825234ee6
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-12-14 15:01:54 +0600

    added login and logout with authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Observable ,  BehaviorSubject } from 'rxjs';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule } from '@angular/router'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { BsNavbarComponent } from './navbar/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { ProductsComponent } from './component/products/products.component';
+import { ShoppingCartComponent } from './component/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './component/check-out/check-out.component';
+import { OrderSuccessComponent } from './component/order-success/order-success.component';
+import { MyOrdersComponent } from './component/my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './component/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    NgbModule.forRoot(),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path:'products', component: ProductsComponent},
+      {path:'shopping-cart', component: ShoppingCartComponent},
+      {path:'check-out', component: CheckOutComponent},
+      {path:'my/orders', component: MyOrdersComponent},
+      {path:'order-success', component: OrderSuccessComponent},
+      {path:'login', component: LoginComponent},
+      {path:'admin/products', component: AdminProductsComponent},
+      {path:'admin/orders', component: AdminOrdersComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC3dr8VQaHl4H5KtiEaw_BoIGoPyBOebwU",
+    authDomain: "bigbangshop-c6b09.firebaseapp.com",
+    databaseURL: "https://bigbangshop-c6b09.firebaseio.com",
+    projectId: "bigbangshop-c6b09",
+    storageBucket: "",
+    messagingSenderId: "890473102521"
+  }
 };
