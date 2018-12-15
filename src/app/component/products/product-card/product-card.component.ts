f9c696be7174647584e203bfa33960528a0e3e8e
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-12-15 15:17:48 +0600

    add items in shopping carts

--- src/app/component/products/product-card/product-card.component.ts
+++ src/app/component/products/product-card/product-card.component.ts
@@ -7,23 +7,13 @@ import { ShoppingCartService } from '../../../services/shopping-cart.service';
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.css']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent {
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
 
   constructor(private cartService: ShoppingCartService) { }
 
   addToCart(product: Product){
-    let cartId = localStorage.getItem('cartId');
-    if(!cartId){
-      this.cartService.create().then(result => {
-        localStorage.setItem('cartId', result.key);
-      });
-    }
-    else{}
+    this.cartService.addToCart(product);
   }
-
-  ngOnInit() {
-  }
-
 }
