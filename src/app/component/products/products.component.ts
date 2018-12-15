a9de546e2a536ffd06788d57497fc6b7c1d03ae2
Rakib31 <30778508+Rakib31@users.noreply.github.com>
2018-12-15 15:45:25 +0600

    dispalaying quantity of the products in cart

--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { CategoryService } from '../../services/category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../models/product';
 import { switchMap } from 'rxjs/operators';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent{
+export class ProductsComponent implements OnInit, OnDestroy{
   products: Product[] = [];
   filteredProducts: Product[] = [];
   categories$;
   category: string;
+  cart: any;
+  subscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
-    private productService: ProductService) { 
+    private productService: ProductService,
+    private shoppingCartService: ShoppingCartService) { 
 
     productService.getAll()
     .pipe(switchMap(products => {
@@ -31,6 +36,15 @@ export class ProductsComponent{
         this.products.filter(p => p.category === this.category) :
         this.products;
     });
+  }
+
+  async ngOnInit(){
+    this.subscription = (await this.shoppingCartService.getCart())
+      .subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
